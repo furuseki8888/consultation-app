31193129d2f6aa82198274addb0f7db522d75887
furuseki8888 <dassentv0202@gmail.com>
2020-08-19 15:06:35 +0900

    profileテーブルの作成

--- db/migrate/20200819050820_create_profiles.rb
+++ db/migrate/20200819050820_create_profiles.rb
@@ -1,7 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.string :nickname, null:false
+      t.integer :prefecture_id
+      t.text :introduction
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_033550) do
+ActiveRecord::Schema.define(version: 2020_08_19_050820) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.integer "prefecture_id"
+    t.text "introduction"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_08_19_033550) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
