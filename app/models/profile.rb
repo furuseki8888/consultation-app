ba0d066330668ae309bbeff9508aaf94ee366618
furuseki8888 <dassentv0202@gmail.com>
2020-08-19 18:36:35 +0900

    ウィザード形式の実装

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to :user, optional :true
-  
+  belongs_to :user, optional: true
+
   validates :nickname, presence: true
 end
