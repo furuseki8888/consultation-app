373d1c14e1dfee57f2455265d6fd23f6693d3671
furuseki8888 <dassentv0202@gmail.com>
2020-08-19 15:25:42 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :profile, dependent: :destroy
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
 end
