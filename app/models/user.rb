21f640a0016438eddea9229ea0a5f36eeeee5887
furuseki8888 <dassentv0202@gmail.com>
2020-08-21 10:03:51 +0900

    アソシエーション、バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one :profile, dependent: :destroy
+  has_many :posts
+
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
 end
