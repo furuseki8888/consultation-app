7610de33a7b69f4790ade805c6726ddff19ef9a0
furuseki8888 <dassentv0202@gmail.com>
2020-08-22 18:55:01 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   
   has_one :profile, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
