ca30796ac45e07f2337ebfe8d6e17104f7081cfc
furuseki8888 <dassentv0202@gmail.com>
2020-08-22 10:28:28 +0900

    comments/createアクションの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    if @post.save
+      redirect_to "/posts/#{comment.post.id}"
+    else
+      render "/posts/#{comment.post.id}"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
