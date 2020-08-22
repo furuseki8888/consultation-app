8eee16f8829dfbdf3a62daeaa52d47b6896a2dd9
furuseki8888 <dassentv0202@gmail.com>
2020-08-22 23:15:09 +0900

    profilesテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_094515) do
+ActiveRecord::Schema.define(version: 2020_08_22_141322) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_094515) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
