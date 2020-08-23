72cfa940deb288ea75152514dcf7042a578290b5
furuseki8888 <dassentv0202@gmail.com>
2020-08-23 13:24:22 +0900

    本人のみがプロフィール編集できるようにする

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   def edit
     @profile = Profile.find(params[:id])
   end
@@ -16,5 +17,12 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:nickname, :prefecture_id, :introduction, :image).merge(user_id: current_user.id)
+  end
+
+  def ensure_correct_user
+    @profile = Profile.find_by(id: params[:id])
+    if @profile.user_id != current_user.id
+      redirect_to root_path, notice: '権限がありません'
+    end
   end
 end
