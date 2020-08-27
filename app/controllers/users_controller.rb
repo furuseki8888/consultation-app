acac28b127de893b4d3c794fb691c9336bebb863
furuseki8888 <dassentv0202@gmail.com>
2020-08-27 14:31:19 +0900

    URLを直接入力して編集できないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order('created_at DESC')
@@ -16,7 +18,15 @@ class UsersController < ApplicationController
     end
   end
 
+private
+
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :password)
+  end
+
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      redirect_to root_path, alert: '権限がありません'
+    end
   end
 end
