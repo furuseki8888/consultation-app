2767e8465d797da7a3ac20b2bc0c3b1345920df3
furuseki8888 <dassentv0202@gmail.com>
2020-08-27 14:22:22 +0900

    users/edit・updateアクションの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: "ユーザー情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :password)
+  end
 end
